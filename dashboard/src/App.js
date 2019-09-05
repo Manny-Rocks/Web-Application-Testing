28a10d3b03bf55443d03160f5514cebf7b841950
Emmanuel Tanifum <etantanifum@gmail.com>
2019-09-05 16:21:48 -0400

    added components/ set state

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
 import './App.css';
+
+
+const [strikes, setStrikes] = useState(0);
+const [balls, setBalls] = useState(0);
+
+const reset = () => {
+  setStrikes(0);
+  setBalls(0);
+}
 
 function App() {
   return (
     <div>
       <h1>SCOREBOARD</h1>
+      <Dashboard reset={reset} strikes={strikes} balls={balls} setStrikes={setStrikes} setBalls={setBalls} />
+      <Display strikes={strikes} balls={balls} />
     </div>
   );
 }
